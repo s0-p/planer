964f43ae04bda90a4ac4183066e668cebf1dc3dd
박소영 <b00v0429@gmail.com>
2022-09-08 22:19:20 +0900

    init

---
+++ src/main/java/com/sop/planer/PlanerApplication.java
@@ -0,0 +1,13 @@
+package com.sop.planer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanerApplication.class, args);
+	}
+
+}
